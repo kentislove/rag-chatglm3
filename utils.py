d2d9657b7e1604ca2eefbb3a8ac48e83d92a924a
kentislove <newlife691027@gmail.com>
2025-05-15 18:02:04 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import os
 import io
 from typing import List
 from langchain.schema import Document
-from langchain.document_loaders import (
+from langchain_community.document_loaders import (
     TextLoader,
     UnstructuredPDFLoader,
     UnstructuredExcelLoader
