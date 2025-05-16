03612ce4619fe2e5376893b9740f368b6b140c59
kentislove <newlife691027@gmail.com>
2025-05-16 09:15:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 import gradio as gr
 
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
